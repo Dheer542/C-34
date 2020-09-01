358ca6763eee061bde6abc1ccab57cb57ed48659
Dheer542 <karuhars@gmail.com>
2020-09-01 16:55:04 +0530

    Synced the ball movement

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
 var ball;
-
+var database, position;
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var node = database.ref('ball/position');
+    node.on("value", readPos, showError);
 }
-
+function readPos(data) {
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showError() {
+    console.log("Error in reading the database");
+}
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        x:position.x+x,
+        y:position.y+y
+    })
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+    
 }
